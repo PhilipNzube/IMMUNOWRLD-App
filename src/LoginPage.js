d67b0381d9953b2052a29513a87c932d29694f6e
PhilipNzube <72675495+PhilipNzube@users.noreply.github.com>
2022-06-29 04:35:40 +0100

    Add files via upload

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -307,7 +307,7 @@ let url=null;
 <label id="InfoID2"><center>Confirm your password</center></label>
 <br/><br/><br/><br/>
 <center><input id="ActivationcodeID2" type="password" placeholder="Enter Previous Password" title="Password must contain: Minimum 8 characters atleast 1 Alphabet and 1 Number" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" required/></center>
-<br/><center><input id="ActivationcodeID3" type="password" placeholder="New PassWord" title="Password must contain: Minimum 8 characters atleast 1 Alphabet and 1 Number" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" required/><div id="PswVisible">
+<br/><center><input id="ActivationcodeID3" type="password" placeholder="New PassWord" title="Password must contain: Minimum 8 characters atleast 1 Alphabet and 1 Number" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" required/><div id="PswVisibleb">
 <div id="PV2"><input id="PswVisible2" type="checkbox" onClick={ShowPswd2}/></div><div id="SP1">SHOWPASSWORD</div></div></center><br/>
 <a href={url} id="CloseButton" onClick={CLOSE}>CLOSE</a>
 <center><div id="SubmitBG2">
